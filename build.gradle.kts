567166c895750add06415c851b38a4d65ec59c74
Vladislav Metelyagin <v.metelyagin@just-ai.com>
2021-04-07 16:36:52 +0300

    update ktor version compatible with jaicf

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
     application
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 group = "com.justai.jaicf"
 version = "1.0.0"
 
-val jaicf = "0.13.0"
+val jaicf = "1.0.0"
 val slf4j = "1.7.30"
-val ktor = "1.4.0"
+val ktor = "1.5.1"
 
 application {
     mainClassName = "com.justai.jaicf.template.ServerKt"
@@ -27,8 +27,8 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:$slf4j")
     implementation("org.slf4j:slf4j-log4j12:$slf4j")
 
-    implementation("com.justai.jaicf:core:$jaicf")
-    implementation("com.justai.jaicf:google-actions:$jaicf")
+    implementation("com.just-ai.jaicf:core:$jaicf")
+    implementation("com.just-ai.jaicf:google-actions:$jaicf")
     implementation("io.ktor:ktor-server-netty:$ktor")
 }
 
